ab8f15459cf46915688285a9c82304494156f818
shef9109 <paint.sans@mail.ru>
2025-05-03 19:57:41 +0300

    Update database

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, TIMESTAMP, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+from typing import List
 
 from app.database.db import Base
 
@@ -11,3 +14,13 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     #otp_secret = Column(String, nullable=True)
+    passwords:Mapped[List["AppPasswords"]] = relationship()
+
+class AppPasswords(Base):
+    __tablename__ = "app_passwords"
+    
+    id:Mapped[int] = mapped_column(primary_key=True)
+    user_id:Mapped[int] = mapped_column(ForeignKey('users.id'))
+    hashed_password:Mapped[str]
+    name_password:Mapped[str]
+    created_at:Mapped[datetime] = mapped_column(TIMESTAMP, server_default=func.now())
